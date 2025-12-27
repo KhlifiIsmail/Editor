07ffc273f6cb2d494d9bb715766baaa08e600a34
Ismail Khlifi <ismail.khliffi@gmail.com>
2025-12-27 12:27:54 +0100

    chore: add Monaco Editor component scaffolding

---
+++ projects/monaco-pattern-editor/src/lib/components/monaco-editor/monaco-editor.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mpe-monaco-editor',
+  imports: [],
+  templateUrl: './monaco-editor.html',
+  styleUrl: './monaco-editor.css',
+})
+export class MonacoEditor {
+
+}
